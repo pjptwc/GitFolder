dee0f1aa315b7e78260868176dcfab63e43ff8c8
Pratikbhai Patel <pratikpatel@me.com>
2016-01-25 18:57:54 -0500

    This is a commit for my addition of two things

--- GitPlayground.playground/Contents.swift
+++ GitPlayground.playground/Contents.swift
@@ -2,4 +2,14 @@
 
 import UIKit
 
-var str = "Hello, playground"
+ var str = "Hello, playground"
+ 
+ func addTwoThings(thing1: Int, thing2: Int) -> Int {
+    return thing1 + thing2
+ }
+ 
+ 
+ // when i add 1 and 2, it should equal 3
+ addTwoThings(1, thing2:2)
+ addTwoThings(2, thing2: 4)
+ addTwoThings(0, thing2: 0)
